7896f81b0b721a38045e6d3beb6662646a7dbf7f
Fitra Aziz <fitradevelopers71@gmail.com>
2018-12-18 14:04:32 +0700

    deploy heroku

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Http\Request;
 
-Route::post('auth/register', 'AuthController@register');
-Route::post('auth/login', 'AuthController@login');
-Route::get('auth/logout', 'AuthController@logout');
-Route::get('user', 'UserController@getAuthUser');
-Route::get('users', 'UserController@users');
-Route::post('user/upload', 'UserController@upload');
-Route::put('user/update', 'UserController@update');
+
+Route::group(['prefix' => 'v1', 'middleware' => 'cors'],
+	function() {
+		// Authentication
+		Route::post('auth/register', 'AuthController@register');
+		Route::post('auth/login', 'AuthController@login');
+		Route::get('auth/logout', 'AuthController@logout');
+
+		// user
+		Route::get('user', 'UserController@getAuthUser');
+		Route::get('users', 'UserController@users');
+		Route::post('user/upload', 'UserController@upload');
+		Route::put('user/update', 'UserController@update');
+	});
+
