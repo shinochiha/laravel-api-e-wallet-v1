6c83fc404e9d96a25cefd239d3ecc6baeb9d56c1
Fitra Aziz <fitradevelopers71@gmail.com>
2018-12-19 11:50:25 +0700

    added Forgot password fix 1

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'fitraaziz71@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'superwallet22@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'SuperWallet'),
     ],
 
