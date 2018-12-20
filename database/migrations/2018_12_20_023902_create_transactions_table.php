d048fb38658294033b9e02dec6b79106af6bc026
Fitra Aziz <fitradevelopers71@gmail.com>
2018-12-20 09:53:41 +0700

    added table transactions

---
+++ database/migrations/2018_12_20_023902_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('transaction_id');
+            $table->enum('type', ['income','expense']);
+            $table->enum('category', ['family', 'sport', 'loan']);
+            $table->bigInteger('amount');
+            $table->string('note');
+            $table->date('date');
+            $table->string('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
