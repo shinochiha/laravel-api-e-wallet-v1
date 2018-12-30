2e263e574467889bfae9286d95ff544380b849e2
Fitra Aziz <fitradevelopers71@gmail.com>
2018-12-30 11:02:50 +0700

    update fix update

--- app/Http/Requests/UpdateRequest.php
+++ app/Http/Requests/UpdateRequest.php
@@ -24,8 +24,8 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'        => 'required|string|min:5|max:50',
-            'phone_number'=> 'numeric|min:11'
+            'name'        => 'required|string|min:5|max:30',
+            'phone_number'=> 'numeric|min:11|max|13'
         ];
     }
 }
