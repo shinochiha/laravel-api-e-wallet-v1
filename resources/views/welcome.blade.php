cbbb9eb4efa835b20afca2a91dc03c016bceb565
Fitra Aziz <fitradevelopers71@gmail.com>
2018-12-30 23:07:51 +0700

    update index laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Endpoint Simple E-Wallet 
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://documenter.getpostman.com/view/5830116/RznBMzgz">Documentation</a>
+                    <a href="https://github.com/shinochiha/laravel-api-e-wallet-v1">GitHub</a>
                 </div>
             </div>
         </div>
